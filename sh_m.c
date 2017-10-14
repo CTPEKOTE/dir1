74dd413345ee0e1a029dc79fa88f618bde6720a7
CTPEKOTE <kirin9@mail.ru>
2017-10-14 18:14:42 +0300

    https://github.com/CTPEKOTE/dir1.git

--- sh_m.c
+++ sh_m.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-//#include <unistd.h>
+#include <unistd.h>
 #include <errno.h>
 #include <sys/sem.h>
-#include <sys/ipc.h>
-#include <sys/types>
+#include <sys/ipc.h>                                            
 #include <sys/shm.h>
-
+#define SIZE 100
 int main(){
-	int *array;
+	char array[SIZE];
 	int shmid;
 	int is_new = 1;
-	key_t key = 123;
-	/*shmget is to create and get adress
-	 * shmat is to attach memory to adress space
-	 * shmdt is to detach memory
-	 * shmctl is to manage memory*/
-		
+	int i = 0;
+	key_t key = 322;
+	if ((shmid = shmget(key, SIZE , 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
+		if(errno != EEXIST){
+			printf("Can't create shared memory\n");
+                        exit(-1);
+                } else {
+                        if((shmid = shmget(key, SIZE  , 0)) < 0) {
+                                printf("Can't find shared memory\n");
+                                exit(-1);
+                        }
+                        is_new = 0;
+                }
+        }
+
+        if ((array = (char *)shmat(shmid, NULL, 0)) == (char *)(-1)) {
+                printf("Can't attach shared memory\n");
+                exit(-1);
+        }
+
+	while(scanf("%c",&(array[i]) != "\n" && i < SIZE - 1){
+                i++;
+        }
+	array[i + 1] = 0;
+	
+
+	if(shmdt(array) < 0){
+                printf("Can't detach shared memory\n");
+                exit(-1);
+        }
 	return 0;
 }
-
